0af2c36085a80394be3ccdf01684d06c76677b4f
logan.priyank <logan.priyank@rubicotech.in>
2025-10-30 15:25:45 +0530

    feat: Add application-flow component with routing setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'application-flow',
+    pathMatch: 'full'
+  },
+  {
+    path: 'application-flow',
+    loadComponent: () => import('./features/application-flow/application-flow').then(m => m.ApplicationFlow)
+  }
+];
---
+++ src/app/features/application-flow/application-flow.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-application-flow',
+  imports: [],
+  templateUrl: './application-flow.html',
+  styleUrl: './application-flow.css',
+})
+export class ApplicationFlow {
+
+}
