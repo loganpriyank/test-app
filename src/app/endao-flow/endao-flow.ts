19343556742109b32f09d3b93c94eadb460f9c0c
logan.priyank <logan.priyank@rubicotech.in>
2025-10-30 15:16:29 +0530

    Initial commit: Angular app with endao-flow component

---
+++ src/app/endao-flow/endao-flow.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-endao-flow',
+  imports: [],
+  templateUrl: './endao-flow.html',
+  styleUrl: './endao-flow.css',
+})
+export class EndaoFlow {
+
+}
